4eb4aedb1b33cd48b465e5a9de6242d5b97ecbea
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2023-09-10 22:23:09 +0900

    페이지네이션 수정

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -25,7 +25,7 @@ const Pagination = () => {
               href="#"
               className="relative inline-flex items-center rounded-l-md px-4 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
-              <span>{"<"}</span>
+              <span className="scale-x-150">{"<"}</span>
               <span className="sr-only">Previous</span>
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-white ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
@@ -73,7 +73,7 @@ const Pagination = () => {
               href="#"
               className="relative inline-flex items-center rounded-r-md px-4 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
-              <span>{">"}</span>
+              <span className="scale-x-150">{">"}</span>
               <span className="sr-only">Next</span>
             </a>
           </nav>
